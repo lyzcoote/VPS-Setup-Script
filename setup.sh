235cc20249f3ff774a4f1816fe4dd82f29b30477
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2022-09-09 09:59:42 +0200

    Update setup.sh
    - Major rewrite of the detection of the OS
    - Added support for major RHEL based distros and Debian based

--- setup.sh
+++ setup.sh
@@ -1,5 +1,94 @@
 #!/bin/bash
 
+################################################################################
+#                                                                              #
+#                                  Functions                                   #
+#                                                                              #
+################################################################################
+
+function checkOS() {
+	if [[ -e /etc/debian_version ]]; then
+		OS="debian"
+		source /etc/os-release
+
+		if [[ $ID == "debian" || $ID == "raspbian" ]]; then
+			if [[ $VERSION_ID -lt 9 ]]; then
+				echo "⚠️ Your version of Debian is not supported."
+				echo ""
+				echo "However, if you're using Debian >= 9 or unstable/testing then you can continue, at your own risk."
+				echo ""
+				until [[ $CONTINUE =~ (y|n) ]]; do
+					read -rp "Continue? [y/n]: " -e CONTINUE
+				done
+				if [[ $CONTINUE == "n" ]]; then
+					exit 1
+				fi
+			fi
+		elif [[ $ID == "ubuntu" ]]; then
+			OS="ubuntu"
+			MAJOR_UBUNTU_VERSION=$(echo "$VERSION_ID" | cut -d '.' -f1)
+			if [[ $MAJOR_UBUNTU_VERSION -lt 16 ]]; then
+				echo "⚠️ Your version of Ubuntu is not supported."
+				echo ""
+				echo "However, if you're using Ubuntu >= 16.04 or beta, then you can continue, at your own risk."
+				echo ""
+				until [[ $CONTINUE =~ (y|n) ]]; do
+					read -rp "Continue? [y/n]: " -e CONTINUE
+				done
+				if [[ $CONTINUE == "n" ]]; then
+					exit 1
+				fi
+			fi
+		fi
+	elif [[ -e /etc/system-release ]]; then
+		source /etc/os-release
+		if [[ $ID == "fedora" || $ID_LIKE == "fedora" ]]; then
+			OS="fedora"
+		fi
+		if [[ $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
+			OS="centos"
+			if [[ ! $VERSION_ID =~ (7|8|9) ]]; then
+				echo "⚠️ Your version of CentOS is not supported."
+				echo ""
+				echo "The script only support CentOS 7 and CentOS 8."
+				echo ""
+				exit 1
+			fi
+		fi
+		if [[ $ID == "ol" ]]; then
+			OS="oracle"
+			if [[ ! $VERSION_ID =~ (8) ]]; then
+				echo "Your version of Oracle Linux is not supported."
+				echo ""
+				echo "The script only support Oracle Linux 8."
+				exit 1
+			fi
+		fi
+		if [[ $ID == "amzn" ]]; then
+			OS="amzn"
+			if [[ $VERSION_ID != "2" ]]; then
+				echo "⚠️ Your version of Amazon Linux is not supported."
+				echo ""
+				echo "The script only support Amazon Linux 2."
+				echo ""
+				exit 1
+			fi
+		fi
+	elif [[ -e /etc/arch-release ]]; then
+		OS=arch
+	else
+		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Amazon Linux 2, Oracle Linux 8 or Arch Linux system"
+		exit 1
+	fi
+}
+
+################################################################################
+#                                                                              #
+#                                  Main Code                                   #
+#                                                                              #
+################################################################################
+
+checkOS
 
 echo -ne "
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
@@ -57,12 +146,20 @@ UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 # If Linux, try to determine specific distribution
 if [ "$UNAME" == "linux" ]; then
     # If available, use LSB to identify distribution
-    RAW_DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-    if [[ $RAW_DISTRO==Arch* ]]; then
-        echo "Running $RAW_DISTRO"
+    if [[ $OS =~ (arch) ]]; then
         pacman -Syy
         pacman -Syu --noconfirm
         pacman -S htop vim nano wget curl git ncdu zsh --noconfirm
+    elif [[ $OS =~ (debian|ubuntu) ]]; then
+        apt update
+        apt upgrade -y
+        apt install htop vim nano wget curl git ncdu zsh -y
+    elif [[ $OS =~ (centos|amzn|oracle) ]]; then
+        yum update -y
+        yum install htop vim nano wget curl git ncdu zsh -y
+    elif [[ $OS =~ (fedora) ]]; then
+        dnf update -y
+        dnf install htop vim nano wget curl git ncdu zsh -y
     else
         echo "Can't detect current distro!"
         exit 1
