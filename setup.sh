17cc1853a5ffe60ac5f768e747e567324bc97aad
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2023-02-07 09:20:55 +0100

    Update setup.sh
    Functions with dialog
    Less code overall

--- setup.sh
+++ setup.sh
@@ -46,6 +46,150 @@ function checkOS() {
 	fi
 }
 
+function installDocker() {
+	clear
+	echo "Downloading Docker setup script..."
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sh get-docker.sh
+	clear
+	echo "You want to add $USERNAME to the Docker group? "
+	echo "(Y/n): "
+	read RESPONSE
+	if [[ $RESPONSE == [Yy]* ]]; then
+	    usermod -aG docker $USERNAME
+	    echo "$USERNAME added to the Docker group"
+	fi
+}
+
+function dockerDialog() {
+	DIALOG=${DIALOG=dialog}
+
+	cmd=($DIALOG --title "Install Docker" --menu "Select options:" 0 0 0)
+
+	options=(1 "Docker Standalone"
+	         2 "Docker + Portainer CE"
+	         3 "Docker + Portainer EE"
+	         4 "Docker + Portainer Agent")
+
+	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
+	for choice in $choices
+	do
+	    case $choice in
+	        1)
+	            echo "First Option | Standalone"
+	            installDocker
+	            ;;
+	        2)
+	            echo "Second Option | Portainer CE"
+	            installDocker
+				installPortainer "CE"
+	            ;;
+	        3)
+	            echo "Third Option | Portainer EE"
+	            installDocker
+				installPortainer "EE"
+	            ;;
+	        4)
+	            echo "Fourth Option | Portainer Agent"
+	            installDocker
+				installPortainer "Agent"
+	            ;;
+	    esac
+	done
+}
+
+function updateNdUpgrade() {
+	# Show the loading bar for apt-get update
+	(
+	  apt-get update 2>&1 | dialog --title "Working" --gauge "Updating packages sources, please wait..." 7 70 0
+	) | dialog --title "Working" --gauge "Updating packages sources, please wait..." 7 70 20
+
+	# Check if the command execution was successful
+	if [ $? -ne 0 ]; then
+	  dialog --title "Error!" --msgbox "Update failed!" 5 30
+	  exit 1
+	fi
+
+	# Show the loading bar for apt-get upgrade
+	(
+	  apt-get upgrade -y 2>&1 | dialog --title "Working" --gauge "Upgrading packages, please wait..." 7 70 20
+	) | dialog --title "Working" --gauge "Upgrading packages, please wait..." 7 70 40
+
+	# Check if the command execution was successful
+	if [ $? -ne 0 ]; then
+	  dialog --title "Error!" --msgbox "Upgrade failed!" 5 30
+	  exit 1
+	fi
+
+	# Show the loading bar for apt-get install
+	(
+	  apt-get install neofetch htop vim nano wget curl git ncdu zsh snapd -y 2>&1 | dialog --title "Working" --gauge "Installing packages, please wait..." 7 70 40) | dialog --title "Working" --gauge "Installing packages, please wait..." 7 70 90
+
+	# Check if the command execution was successful
+	if [ $? -ne 0 ]; then
+	  dialog --title "Error!" --msgbox "Installation failed!" 5 30
+	  exit 1
+	fi
+
+	# Show success message
+	dialog --title "Done!" --msgbox "System update and installation successful!" 6 30
+	clear
+}
+
+function upgradeDialog() {
+	DIALOG=${DIALOG=dialog}
+
+	$DIALOG --title "Update and Upgrade System" --clear \
+	        --yesno "Would you like to update and upgrade the system?" 0 0 
+
+	case $? in
+	  0)
+	    updateNdUpgrade
+	    ;;
+	  1)
+	    echo "No chosen.";;
+	  255)
+	    echo "ESC pressed.";;
+	esac
+}
+
+function showDialog() {
+	DIALOG=${DIALOG=dialog}
+
+	$DIALOG --title "$1" --clear \
+	        --yesno "$2" 0 0 
+
+	case $? in
+	  0)
+	    $3
+	    ;;
+	  1)
+	    echo "No chosen.";;
+	  255)
+	    echo "ESC pressed.";;
+	esac
+}
+
+function installPortainer() {
+	portainerType=$1
+	if [[ $portainerType == "CE" ]]; then
+		echo "## Installing Portainer CE..."
+		docker volume create portainer_data
+		docker run -d -p 9443:9443 -p 8000:8000 --name portainer_ce --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_stuff:/data portainer/portainer-ce:latest
+	elif [[ $portainerType == "EE" ]]; then
+		echo "## Installing Portainer EE..."
+		docker volume create portainer_data
+		docker run -d -p 9443:9443 -p 8000:8000 --name portainer_ee --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_stuff:/data portainer/portainer-ee:latest
+	elif [[ $portainerType == "Agent" ]]; then
+		echo "## Installing Portainer Agent..."
+		docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.16.2
+	else
+		echo "[!] Invalid Portainer type";
+		exit 1;
+	fi
+}
+
 ################################################################################
 #                                                                              #
 #                                  Main Code                                   #
@@ -61,6 +205,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+apt install -y dialog
+
+clear
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -113,9 +260,7 @@ UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 # If Linux, try to determine specific distribution
 if [ "$UNAME" == "linux" ]; then
     if [[ $OS =~ (debian|ubuntu) ]]; then
-        apt update
-        apt upgrade -y
-        apt install htop vim nano wget curl git ncdu zsh nginx snapd -y
+        upgradeDialog
     else
         echo "Can't detect current distro!"
         exit 1
@@ -130,150 +275,16 @@ clear
 
 echo -ne "
 -------------------------------------------------------------------------
-                Installing additional QOF packages
--------------------------------------------------------------------------
-"
-## Additional packages for Debian/Ubuntu
-if [[ $OS =~ (debian|ubuntu) ]]; then
-	echo "Would you like to install Nala? (Nice front-end for apt)"
-	echo "(Y/n)? "
-	read RESPONSE
-    if [[ $RESPONSE == [Yy]* ]]; then
-		echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-		wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-		if [[ $OS =~ (debian) ]] && [[ $VERSION_CODENAME = "bullseye" ]]; then
-			apt update
-			apt install nala-legacy -y
-		elif [[ $OS =~ (debian) ]] && [[ $VERSION_CODENAME =~ (testing|bookworm|sid) ]]; then
-			apt update
-			apt install nala -y
-		elif [[ $OS =~ (ubuntu) ]] && [[ $VERSION_ID = "22.04" ]]; then
-			apt update
-			apt install nala -y
-		elif [[ $OS =~ (ubuntu) ]] && [[ $VERSION_ID =~ (21.04|20.04) ]]; then
-			apt update
-			apt install nala-legacy -y
-		else
-			echo "Cannot find a proper canditate OS for Nala or Nala-legacy :/"
-		fi
-	else
-		echo "Not installing Nala/Nala-Legacy."
-	fi
-fi
-
-clear
-
-echo -ne "
--------------------------------------------------------------------------
-                   Installing docker and portainer
+                   Installing Docker and Portainer
 -------------------------------------------------------------------------
 "
 ## Necessary repo, packages and config
 
 if [[ $OS =~ (debian|ubuntu) ]]; then
-	echo "Would you like to install Docker and Portainer? (Nice front-end web gui for docker)"
-	echo "(Y/n)? "
-	read RESPONSE
-    if [[ $RESPONSE == [Yy]* ]]; then
-		echo "Downloading Docker setup script..."
-		curl -fsSL https://get.docker.com -o get-docker.sh
-		sh get-docker.sh
-		# Installing Portainer
-		read -p "Press enter to continue"
-		clear
-		echo "## Installing Portainer..."
-		docker volume create portainer_data
-		docker run -d -p 9443:9443 -p 8000:8000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_stuff:/data portainer/portainer-ce:latest
-		# Installing Netdata
-		read -p "Press enter to continue"
-		clear
-		echo "## Installing Netdata..."
-		docker run -d --name=netdata -p 19999:19999 -v netdataconfig:/etc/netdata -v netdatalib:/var/lib/netdata -v netdatacache:/var/cache/netdata -v /etc/passwd:/host/etc/passwd:ro -v /etc/group:/host/etc/group:ro -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /etc/os-release:/host/etc/os-release:ro --restart unless-stopped --cap-add SYS_PTRACE --security-opt apparmor=unconfined netdata/netdata
-	else 
-		echo "Not installing Docker and Portainer"
-	fi
+	dockerDialog
 fi
 
 clear
-
-echo -ne "
--------------------------------------------------------------------------
-                   	Configuring Nginx's Web Servers
--------------------------------------------------------------------------
-"
-
-echo "Would you like to configure and deploy the Nginx's Web Servers for"
-echo "Portainer, NetData"
-echo "(Y/n)? "
-read RESPONSE
-if [[ $RESPONSE == [Yy]* ]]; then
-	echo "## Creating the virtual host and deploying it"
-	# Creating the site
-	NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
-
-	# Create nginx config file
-	cat > $NGINX_AVAILABLE_VHOSTS/net.lyzcoote.gay.conf <<EOF
-server 
-{
-    listen 80;
-    server_name net.lyzcoote.gay;
-    location / 
-    {
-		proxy_pass http://localhost:19999;
-	}
-}
-EOF
-
-	# Enabling the site
-	cp /etc/nginx/sites-available/net.lyzcoote.gay.conf /etc/nginx/sites-enabled/net.lyzcoote.gay.conf
-
-	# Restart
-	echo "Do you wish to restart nginx?"
-	echo "(Y/n)? "
-	read RESPONSE
-	if [[ $RESPONSE == [Yy]* ]]; then
-	    service nginx restart
-	else
-		echo "Not restating Nginx"
-	fi
-	read -p "Press enter to continue"
-	clear
-
-	# SSL for Netdata and Portainer
-	echo "Do you want to enable SSL to the services created previously?"
-	echo "(Y/n)? "
-	read RESPONSE
-	if [[ $RESPONSE == [Yy]* ]]; then
-		echo "## Installing Certbot for Nginx "
-		snap install core
-		snap refresh core
-		snap install --classic certbot
-		ln -s /snap/bin/certbot /usr/bin/certbot
-		echo "Please input the Certbot Wizard for enable SSL."
-		echo "If needed, re-run 'certbot --nginx' multiple times."
-		echo "## REMEMBER TO CHECK IF DNS RECORDS FOR 'netdata.' AND 'portainer.' MATCH THE IP OF THE VPS"
-		read -p "Press enter to continue"
-		clear
-		certbot --nginx
-		clear
-		echo "Netdata Site Created and deployed on: net.lyzcoote.gay"
-	else 
-		echo "Do you want to install certbot for Nginx?"
-		echo "(Y/n)? "
-		read RESPONSE
-		if [[ $RESPONSE == [Yy]* ]]; then
-			echo "## Installing Certbot for Nginx "
-			snap install core
-			snap refresh core
-			snap install --classic certbot
-			ln -s /snap/bin/certbot /usr/bin/certbot
-		else 
-			echo "Not installing Certbot"
-		fi
-	fi
-else 
-	echo "You may need to configure the Nginx's Virtual Hosts manually"	
-fi
 
 
 echo -ne "
